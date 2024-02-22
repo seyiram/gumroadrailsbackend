abae4ebe7f2b7ccce6c0d9e839759ed88d53f1f6
seyiram <yoggi.jerry@gmail.com>
2024-02-22 03:30:30 -0600

    Modify open_ai_controller to return array of urls

--- backend/app/controllers/open_ai_controller.rb
+++ backend/app/controllers/open_ai_controller.rb
@@ -1,5 +1,6 @@
 class OpenAiController < ApplicationController
   require 'openai'
+  require 'net/http'
 
   def generate_image
     client = OpenAI::Client.new(api_key: ENV["OPEN_AI_KEY"])
@@ -9,7 +10,25 @@ class OpenAiController < ApplicationController
         size: params[:size] || '1024x1024'
       }
     )
-    render json: { url: response.dig("data", 0, "url") }
+    urls = response['data'].map { |img| img['url'] }
+    render json: { urls: urls }
+  rescue => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  def serve_image
+    image_url = params[:url]
+
+    # Validate the URL
+
+    uri = URI(image_url)
+    response = Net::HTTP.get_response(uri)
+
+    if response.is_a?(Net::HTTPSuccess)
+      send_data response.body, type: response.content_type, disposition: 'inline'
+    else
+      render json: { error: "Failed to load image" }, status: :bad_gateway
+    end
   rescue => e
     render json: { error: e.message }, status: :internal_server_error
   end
