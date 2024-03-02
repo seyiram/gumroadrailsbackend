d622f249861f9f499b3fa801676a86ed88e37c93
seyiram <yoggi.jerry@gmail.com>
2024-03-02 02:26:28 -0600

    Add user update functionality to users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resource :users, only: [:create]
+  resource :users, only: [:create, :update]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
 
